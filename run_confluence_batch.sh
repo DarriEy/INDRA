c324c37c56a92dfb057f39239db06c9af2c2e05b
Darri Eythorsson <dae5@hi.is>
2024-11-05 13:52:46 -0700

    added run_confluence_batch.sh

---
+++ run_confluence_batch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --output=CONFLUENCE_single_%j.log
+#SBATCH --error=CONFLUENCE_single_%j.err
+#SBATCH --time=20:00:00
+#SBATCH --ntasks=8
+#SBATCH --mem-per-cpu=5G
+
+
+# Activate your Python environment if necessary
+module restore confluence_modules
+
+
+source /home/darri/confluence_env/scienv/bin/activate
+
+# Your commands here
+
+python ../CONFLUENCE/CONFLUENCE.py --config /home/darri/code/INDRA/0_config_files/config_active.yaml
